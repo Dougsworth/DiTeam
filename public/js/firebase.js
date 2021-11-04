39c2c25d9ae7686088674a8e4a7eb0065657eeed
Douglas Byfield <douglasbyfield@yahoo.com>
2021-11-04 14:05:22 -0500

    updating server, and couple more edits

---
+++ public/js/firebase.js
@@ -0,0 +1,12 @@
+let firebaseConfig = {
+  apiKey: "AIzaSyCGXqalgSJ0cV63yJplwyGciUBBfaQSzyA",
+  authDomain: "diteam-blog.firebaseapp.com",
+  projectId: "diteam-blog",
+  storageBucket: "diteam-blog.appspot.com",
+  messagingSenderId: "1053297662924",
+  appId: "1:1053297662924:web:d56dcfb1f4b86315069b69",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
