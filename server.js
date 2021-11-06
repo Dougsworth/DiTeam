ea79324d1c1cf4d7288d0a9ff9eeae74d02d6275
Douglas Byfield <douglasbyfield@yahoo.com>
2021-11-05 22:52:43 -0500

    changes to server

--- server.js
+++ server.js
@@ -2,20 +2,21 @@ const express = require('express');
 const path = require('path');
 const fileupload = require('express-fileupload');
 
-let initial_path = path.join(__dirname, "Public");
+let initial_path = path.join(__dirname, "public");
+
 const app = express();
 app.use(express.static(initial_path));
 app.use(fileupload());
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(initial_path,"./home.html"));
+    res.sendFile(path.join(initial_path, "home.html"));
 })
 
 app.get('/editor', (req, res) => {
     res.sendFile(path.join(initial_path, "editor.html"));
 })
 
-//upload link
+// upload link
 app.post('/upload', (req, res) => {
     let file = req.files.image;
     let date = new Date();
@@ -34,30 +35,23 @@ app.post('/upload', (req, res) => {
         }
     })
 })
-//moving te admin route code above the blog code will not let anything followed by one / be a blog
+
 app.get("/admin", (req, res) => {
     res.sendFile(path.join(initial_path, "dashboard.html"));
-
 })
 
 app.get("/:blog", (req, res) => {
     res.sendFile(path.join(initial_path, "blog.html"));
 })
 
-app.get("/:blog/editor", (req,res) => {
+app.get("/:blog/editor", (req, res) => {
     res.sendFile(path.join(initial_path, "editor.html"));
 })
-
 
 app.use((req, res) => {
     res.json("404");
 })
 
-//changed to run online instead
-// app.listen("3000", () => {
-//     console.log('listening......');
-    
-    app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('listening......');
-
 })
